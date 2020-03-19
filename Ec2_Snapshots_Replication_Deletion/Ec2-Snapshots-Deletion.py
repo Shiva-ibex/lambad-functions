6ece9403e738c764fac1f7a80a6c33e66b6ba55e
Shiva-ibex <55545107+Shiva-ibex@users.noreply.github.com>
2020-03-19 12:59:31 +0530

    Update Ec2-Snapshots-Deletion.py

--- Ec2_Snapshots_Replication_Deletion/Ec2-Snapshots-Deletion.py
+++ Ec2_Snapshots_Replication_Deletion/Ec2-Snapshots-Deletion.py
@@ -9,30 +9,28 @@ import boto3
 import collections 
 import datetime
 #from datetime import datetime
-dbs = boto3.client('rds','us-east-2')
+ec = boto3.client('ec2','us-east-2')
 def lambda_handler(event, context):
     print('hi')
-    reservations = dbs.describe_db_snapshots()
-    print(reservations["DBSnapshots"])
-    #print(reservations)
-     # get the time
-    now = datetime.datetime.today().strftime('%d%m%Y')
+    reservations = ec.describe_snapshots(OwnerIds=["self"])
+    print(reservations["Snapshots"])
+    # get the time
+    now = datetime.datetime.today().strftime('%Y%m%d')
     print ('now')
     current = int(now)
-    #print(current)
-    retention = 2
-    for snapshot in reservations["DBSnapshots"]:
-    #    print(snapshot["DBSnapshotId"])
-        print ("Checking snapshot %s which was created on %s" % (snapshot['DBSnapshotIdentifier'],snapshot['SnapshotCreateTime']))
+    retention = 1
+    for snapshot in reservations["Snapshots"]:
+        #print(snapshot["SnapshotId"])
+        print ("Checking snapshot %s which was created on %s" % (snapshot['SnapshotId'],snapshot['StartTime']))
         # Remove timezone info from snapshot in order for comparison to work below
-        x = snapshot["SnapshotCreateTime"].strftime('%d%m%Y')
+        x = snapshot["StartTime"].strftime('%Y%m%d')
         print(x)
         snaptime = int(x)
-        print (snaptime)
+        print (snaptime) 
         z = current - snaptime
         print(z)
         if z > retention:
             print("The snapshot older than One day. Deleting Now")
-            dbs.delete_db_snapshot(DBSnapshotIdentifier= snapshot['DBSnapshotIdentifier'])
+            ec.delete_snapshot(SnapshotId= snapshot['SnapshotId'])
         else:
             print("Snapshot is newer than configured retention of %d days so we keep it" % (retention))
