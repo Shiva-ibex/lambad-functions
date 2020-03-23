51d9c1a0acee869afcfd749fddbd07f5ec933a6e
Shiva-ibex <55545107+Shiva-ibex@users.noreply.github.com>
2020-03-23 12:42:27 +0530

    Create Rds-Delete-Snapshot.py

---
+++ RDS_Snapshoots_Replication_Deletion/Rds-Delete-Snapshot.py
@@ -0,0 +1,37 @@
+
+## This function deletes the snapshots based on given retention period both at source region and destination region
+
+## It deletes the snapshots based on creation time criteria
+
+
+import boto3 
+import collections 
+import datetime
+#from datetime import datetime
+dbs = boto3.client('rds','us-east-2')
+def lambda_handler(event, context):
+    #print('hi')
+    reservations = dbs.describe_db_snapshots()
+    print(reservations["DBSnapshots"])
+    #print(reservations)
+     # get the time
+    now = datetime.datetime.today().strftime('%d%m%Y')
+    print ('now')
+    current = int(now)
+    #print(current)
+    retention = 2
+    for snapshot in reservations["DBSnapshots"]:
+    #    print(snapshot["DBSnapshotId"])
+        print ("Checking snapshot %s which was created on %s" % (snapshot['DBSnapshotIdentifier'],snapshot['SnapshotCreateTime']))
+        # Remove timezone info from snapshot in order for comparison to work below
+        x = snapshot["SnapshotCreateTime"].strftime('%d%m%Y')
+        print(x)
+        snaptime = int(x)
+        print (snaptime)
+        z = current - snaptime
+        print(z)
+        if z > retention:
+            print("The snapshot older than One day. Deleting Now")
+            dbs.delete_db_snapshot(DBSnapshotIdentifier= snapshot['DBSnapshotIdentifier'])
+        else:
+            print("Snapshot is newer than configured retention of %d days so we keep it" % (retention))
