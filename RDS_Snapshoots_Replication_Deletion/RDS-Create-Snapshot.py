5d8eac736743c35487073eb51e54c5f1eb348886
Shiva-ibex <55545107+Shiva-ibex@users.noreply.github.com>
2020-03-23 12:21:37 +0530

    Create RDS-Create-Snapshot.py

---
+++ RDS_Snapshoots_Replication_Deletion/RDS-Create-Snapshot.py
@@ -0,0 +1,22 @@
+
+// The below function Creates running rds snapshots based on given database environment names
+
+
+
+
+import json
+import os
+import boto3
+import datetime
+import json
+client = boto3.client('rds')
+start_date = str(datetime.datetime.now().strftime("%d-%m-%Y"))
+#region = os.getenv("region")
+def lambda_handler(event, context):
+    dbs = json.loads(os.getenv("dbs"))
+    for db in dbs:
+        print(db)
+        createdbsnapshot = client.create_db_snapshot(DBSnapshotIdentifier='lambdasnapshot-for-'+db+'-'+start_date,DBInstanceIdentifier=db)
+        print(createdbsnapshot)
+        #describingdbsnapshot = client.describe_db_snapshots(DBInstanceIdentifier='inspired')
+        #print(describingdbsnapshot)
